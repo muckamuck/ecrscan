6f8398112fc7386828c05539f61a6517ed4fcf9c
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2020-12-01 13:28:49 -0600

    Bump version: 0.2.1 → 0.2.2

--- ecrscan/command.py
+++ ecrscan/command.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 
 
 @click.group()
-@click.version_option(version='0.2.1')
+@click.version_option(version='0.2.2')
 def cli():
     '''
     A utility for working with ECR image scan results
